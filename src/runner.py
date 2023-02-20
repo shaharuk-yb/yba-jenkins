095559aa1313355d9d863e72157b943055af1fae
shaharuk-yb <sshaikh@yugabyte.com>
2023-02-20 12:29:48 +0530

    fix

--- src/runner.py
+++ src/runner.py
@@ -2,9 +2,8 @@
 import logging
 
 from yba_ops import YBAOps
-from utls import YBA_BASE_URL, YBA_USERNAME, YBA_PASSWORD, aws_1node_1az_rf1
+from utils import YBA_BASE_URL, YBA_USERNAME, YBA_PASSWORD, aws_1node_1az_rf1
 import time
-
 
 if __name__ == "__main__":
     yba = YBAOps({"endpoint": YBA_BASE_URL, "username": YBA_USERNAME, "password": YBA_PASSWORD})
@@ -15,15 +14,20 @@ if __name__ == "__main__":
 
     # create universe with template
     try:
+        logging.info("Creating universe with payload")
         res, universe_url = yba.create_universe(aws_1node_1az_rf1)
         counter = 0
         while counter < 120 and yba.get_task_status(res['taskUUID'])['status'] == 'Running':
             counter += 1
+            logging.info("Waiting for universe {} creation".format(aws_1node_1az_rf1["universeUUID"]))
             time.sleep(60)
 
         task_resp = yba.get_task_status(res['taskUUID'])
     except Exception as e:
         logging.error("Exception while creating universe")
+
+    # print node details
+    logging.info(yba.get_universe_nodes(aws_1node_1az_rf1["universeUUID"]))
 
     # delete universe
     yba.delete_universe("sshaikh-test1")
@@ -31,5 +35,3 @@ if __name__ == "__main__":
         logging.info("Successfully deleted the universe")
     else:
         logging.info("Failed to delete the universe")
-
-
